f0f3324b8b7c013f165df2b11ecf61024098c18c
Kenta Kozuka <kk4649@hotmail.co.jp>
2016-08-20 18:00:01 +0900

    ks

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
-var express = require('express'),
-    app = express();
+var express = require('express');
+var app = express();
 
 app.get('/', function(req, res) {
     res.send('hello world');
 });
 
-app.listen(5000);
-console.log("server running...");
+var port = process.env.PORT || 5000;
+app.listen(port, function() {
+      console.log("Listening on " + port);
+});
