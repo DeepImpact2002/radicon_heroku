5ca61512a2be32170112cac949160385fc79c8df
Kenta Kozuka <kk4649@hotmail.co.jp>
2016-08-28 16:44:40 +0900

    second commit

--- app.js
+++ app.js
@@ -1,8 +1,16 @@
 var express = require('express');
 var app = express();
 
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+
+//middleware
+
+
+
+
 app.get('/', function(req, res) {
-    res.send('hello world');
+    res.render('index', {title: 'Hello!'});
 });
 
 var port = process.env.PORT || 5000;
