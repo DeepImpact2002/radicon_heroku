e9986db7fd308e8e2df9e48e8243f6a28b9fe904
Kenta Kozuka <kk4649@hotmail.co.jp>
2016-08-28 17:48:33 +0900

    from kenta

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
 
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
@@ -8,12 +10,26 @@ app.set('view engine', 'ejs');
 
 
 
-
 app.get('/', function(req, res) {
     res.render('index', {title: 'Hello!'});
 });
 
+// クライアントからの接続を待つ
+io.on('connection', function(socket){
+    console.log('a user connected');
+
+
+    socket.on('disconnect', function(){
+        console.log('user disconnected');
+    });
+    // クライアントからメッセージを受け取ったら投げ返す
+    socket.on('chat message', function(msg){
+        // 同じクライアントに送信する場合は socket.emit を io.emit に変える
+        socket.emit('chat message', msg);
+    });
+});
+
 var port = process.env.PORT || 5000;
-app.listen(port, function() {
+http.listen(port, function() {
       console.log("Listening on " + port);
 });
